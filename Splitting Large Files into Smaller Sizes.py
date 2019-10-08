38d06a97f0a07517321d45992a2ba998c0915073
David Liau <dliau@ucsd.edu>
2019-10-08 10:11:46 -0500

    Version 0.1.2
    Quick bugfixes/error handling

--- Splitting Large Files into Smaller Sizes.py
+++ Splitting Large Files into Smaller Sizes.py
@@ -6,9 +6,9 @@
 # <strong>Author:</strong> David Liau <br />
 # <strong>Date:</strong> 9/18/2019 <br />
 # <strong>Purpose:</strong> This script serves to take in a large file of size > 1GB, and split it into multiple files of size ~1GB. Any suggestions to improve speed/efficiency would be greatly appreciated. <br />
-# <strong>Version:</strong> 0.1.2
+# <strong>Version:</strong> 0.0.0
 
-# In[ ]:
+# In[1]:
 
 
 # Necessary Imports
@@ -16,13 +16,15 @@ from tkinter import Tk
 from tkinter.filedialog import asksaveasfilename, askopenfilename
 
 
-# In[ ]:
+# In[2]:
 
 
 def y_n_question(question):
     while True:
         # Ask question
         answer = input(question)
+        if not answer or answer == '':
+            continue
         answer_cleaned = answer[0].lower()
         if answer_cleaned == 'y' or answer_cleaned == 'n':
             if answer_cleaned == 'y':
@@ -42,7 +44,7 @@ def select_file_in(title):
     return file_in
 
 
-# In[ ]:
+# In[3]:
 
 
 # Confirmation question
@@ -78,12 +80,12 @@ while True:
             continue
             
     except AssertionError:
-        print("Not a valid size. Please enter an integer between 1 byte and 1GB (1000000000 bytes).")
+        print("Not a valid size. Please enter an integer between 1 byte and 1GB (1,000,000,000 bytes).")
     except ValueError:
         print("Not a valid type. Please input an integer.")
-
+        
 # Writing to multiple files
-with open(file_in) as f:
+with open(file_in, encoding='utf-8') as f:
     header = f.readline()
     part = 0
     total = 0
@@ -91,35 +93,43 @@ with open(file_in) as f:
     # Create filename, in the format of: filename_{part}.{file type}
     period_i = file_in.rfind('.')
     file_name = file_in[:period_i] + '_' + str(part) + file_in[period_i:]
-    output_file = open(file_name, 'a+')
+    output_file = open(file_name, 'a+', encoding='utf-8')
     output_file.write(header)
     
     # Reading from input file
-    while 1:        
-        chunk = f.readline()
-        
-        # If end of input file, break
-        if not chunk:
+    while 1:
+        # Trying to read
+        try:
+            chunk = f.readline()
+
+            # If end of input file, break
+            if not chunk:
+                output_file.close()
+                f.close()
+                print("Writing to", file_name, "complete!")
+                break
+
+            # Write to output file while size is still less than limit
+            if total + len(chunk) <= int(size):
+                output_file.write(chunk)
+                total += len(chunk)
+
+            # Close output file and open a new one
+            else:
+                output_file.close()
+                print("Writing to", file_name, "complete!")
+
+                # Updating variables for new otuput file
+                total = 0
+                part += 1
+                file_name = file_in[:period_i] + '_' + str(part) + file_in[period_i:]
+                output_file = open(file_name, 'a+', encoding='utf-8')
+                output_file.write(header)
+        # Catching any exception and printing it, as well as closing files
+        except Exception as exp:
+            print("Program failed with exception: %s" % (exp))
             output_file.close()
-            print("Writing to", file_name, "complete!")
-            break
-         
-        # Write to output file while size is still less than limit
-        if total + len(chunk) <= int(size):
-            output_file.write(chunk)
-            total += len(chunk)
-            
-        # Close output file and open a new one
-        else:
-            output_file.close()
-            print("Writing to", file_name, "complete!")
-            
-            # Updating variables for new otuput file
-            total = 0
-            part += 1
-            file_name = file_in[:period_i] + '_' + str(part) + file_in[period_i:]
-            output_file = open(file_name, 'a+')
-            output_file.write(header)
+            f.close()
 
 
 # In[ ]:
